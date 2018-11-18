e41f39072e0b4e88db51c8f5c4988d7d96edd402
Dan Salmon <salmon@protonmail.ch>
2018-11-18 13:15:23 -0600

    Add status reports and suppress non-errors from S3 upload

--- deployBlog.sh
+++ deployBlog.sh
@@ -13,13 +13,41 @@ YOUR_DOMAIN=""
 #				   #
 ####################################
 
+BASKET='\U1F5D1\UFE0F  '
+CHECKMARK='\U2705  '
+PACKAGE='\U1F4E6  '
+HAMMER='\U1F6E0\UFE0F  '
+
+
 BUCKET_NAME="${YOUR_DOMAIN}-cdn"
+
+printf "${PACKAGE}"
+echo "Cloning packages locally..."
 
 git clone https://github.com/sa7mon/academic-kickstart.git blog
 cd blog/
 git submodule update --init --recursive
+
+printf "${CHECKMARK}"
+echo "Done!"
+echo ""
+
+printf "${HAMMER}"
+echo "Building static site..."
+
 hugo
 
-aws s3 sync --acl "public-read" blog/public/ s3://${BUCKET_NAME} --exclude 'post'
+printf "${CHECKMARK}"
+echo "Done!"
+echo ""
+
+printf "${BASKET}"
+echo "Uploading public site files to S3 bucket..."
+aws s3 sync --acl "public-read" public/ s3://${BUCKET_NAME} --exclude 'post' --only-show-errors
+
+printf "${CHECKMARK}"
+echo "Done!"
+echo ""
+
 
 # aws cloudfront list-distributions --output text --query "DistributionList.Items[].{DomainName: DomainName, Id: Id, OriginDomainName: Origins.Items[0].DomainName}[?contains(OriginDomainName, '${YOUR_DOMAIN}')] | [0].Id"
